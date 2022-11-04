e6d8bb1d990c2cedc2124c4fb68a9462a0bd9095
lana2-2karla <allanakarla1702@gmail.com>
2022-11-04 16:02:30 -0300

    :sprakles: feat: cria função days_never_visited

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,18 +1,22 @@
 from collections import Counter
-import csv, sys
+import csv
+import os
 
 
 def read_path(path_to_file):
 
     requests_list = []
 
+    if path_to_file.split(".")[-1] != "csv":
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+
+    if not os.path.isfile(path_to_file):
+      raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
     with open(path_to_file, 'r') as file:
         reader = csv.reader(file)
-        try:
-            for line in reader:
-                requests_list.append(line)
-        except csv.Error as err:
-            sys.exit('file %s, linha %d: %s' % (file, reader.line_num, err))
+        for line in reader:
+            requests_list.append(line)
     return requests_list
 
 
@@ -55,20 +59,42 @@ def quantity_plates(client_name: str, list_customers: list, plate: str):
 
 
 def dishes_never_ordered(client_name: str, list_customers: list):
-    plates = []
+    set_plates = set()
+    set_plates_costumer = set()
+
 
     for costumer in list_customers:
-        plates.append(costumer['dish'])
-        set_plates = set(plates)
+        set_plates.add(costumer['dish'])
         if costumer['name'] == client_name:
-            print(set_plates.difference(costumer['dish']))
-    
+            set_plates_costumer.add(costumer['dish']) 
+    return set_plates.difference(set_plates_costumer)
+
+def days_never_visited(client_name: str, list_costumers: list):
+
+    set_plates = set()
+    set_plates_costumer = set()
+
+
+    for costumer in list_costumers:
+        set_plates.add(costumer['day'])
+        if costumer['name'] == client_name:
+            set_plates_costumer.add(costumer['day']) 
+    return set_plates.difference(set_plates_costumer)
+        
+
 
 def analyze_log(path_to_file):
     requests_list_dict = turns_into_dict(path_to_file)
-    most_requested_dish('maria', requests_list_dict)
-    quantity_plates('arnaldo', requests_list_dict, 'hamburguer')
-    dishes_never_ordered('joao', requests_list_dict)
+    most_requested_dish_maria = most_requested_dish('maria', requests_list_dict)
+    quantity_plates_arnaldo = quantity_plates('arnaldo', requests_list_dict, 'hamburguer')
+    dishes_never_ordered_joao = dishes_never_ordered('joao', requests_list_dict)
+    days_never_visited_joao = days_never_visited('joao', requests_list_dict)
+
+    with open("data/mkt_campaign.txt", mode="w") as new_file:
+        new_file.write(f"{most_requested_dish_maria}" + "\n")
+        new_file.write(f"{quantity_plates_arnaldo}\n")
+        new_file.write(f"{dishes_never_ordered_joao}\n")
+        new_file.write(f"{days_never_visited_joao}\n")
 
 
 

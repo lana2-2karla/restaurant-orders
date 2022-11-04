f150bc4554a37ab0a690a1b059cb677931c69627
lana2-2karla <allanakarla1702@gmail.com>
2022-11-04 16:56:00 -0300

    :sparkles: feat: implementa métodos da classe TrackOrders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,45 @@
+from collections import Counter
+from analyze_log import days_never_visited, dishes_never_ordered, most_requested_dish
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque - ok
+
+    def __init__(self):
+        self.orders = []
+        
     def __len__(self):
-        pass
+        return len(self.orders)
+        
 
     def add_new_order(self, customer, order, day):
-        pass
+        order = {"name": customer, "dish": order, "order_day": day}
+        self.orders.append(order)
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        return most_requested_dish(customer, self.orders)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        return dishes_never_ordered(customer, self.orders)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        return days_never_visited(customer, self.orders)
 
     def get_busiest_day(self):
-        pass
+
+        days = []
+
+        for costumer in self.orders:
+            days.append(costumer['order_day'])
+
+        counter_days = Counter(days)
+        return max(counter_days, key=counter_days.get)
 
     def get_least_busy_day(self):
-        pass
+
+        days = []
+
+        for costumer in self.orders:
+            days.append(costumer['order_day'])
+
+        counter_days = Counter(days)
+        return min(counter_days, key=counter_days.get)
